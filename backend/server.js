1a980877bbc8565040979fab8dbcf851a5c15f83
bharath Kv <bharathstbk@gmail.com>
2025-04-14 00:23:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Database connection string
-const MONGO_URI = 'mongodb+srv://swarnim21567:Swarnim%4045@passportsewa.lv3ev.mongodb.net/?retryWrites=true&w=majority&appName=passportSewa';
+//const MONGO_URI = 'mongodb+srv://swarnim21567:Swarnim%4045@passportsewa.lv3ev.mongodb.net/?retryWrites=true&w=majority&appName=passportSewa';
+const MONGO_URI = 'mongodb+srv://bharath21362:<db_password>@cluster101.fgcxxte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster101';
 
 // Database connection
 mongoose.connect(MONGO_URI, {
