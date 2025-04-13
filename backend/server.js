3937da4f370d6b748ecfe6118a92fda06704345f
bharath Kv <bharathstbk@gmail.com>
2025-04-14 00:53:45 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ app.use(cors());
 
 // Database connection string
 //const MONGO_URI = 'mongodb+srv://swarnim21567:Swarnim%4045@passportsewa.lv3ev.mongodb.net/?retryWrites=true&w=majority&appName=passportSewa';
-const MONGO_URI = 'mongodb+srv://bharath21362:<db_password>@cluster101.fgcxxte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster101';
+const MONGO_URI = 'mongodb+srv://bharath21362:y6XxGC2vnQNYROM9@cluster101.fgcxxte.mongodb.net/?retryWrites=true&w=majority&appName=Cluster101
+';
 
 // Database connection
 mongoose.connect(MONGO_URI, {
